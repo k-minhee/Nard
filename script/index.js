e939fb51b8533db19b6808cf4e0c9d034afd14b4
minhee <alsgml9154@naver.com>
2025-02-04 16:27:53 +0900

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,9 @@
+const bestSwiper = new Swiper('.b_swiper', {
+    slidesPerView: 3,
+    spaceBetween: 8,
+});
+
+const newSwiper = new Swiper('.new_swiper', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+});
